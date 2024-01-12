5578d621d78969562141d4b226970bdb4b8a1937
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2024-01-12 15:49:44 -0300

    :sparkles: feat: Configurando as view para renderizar o form ajustes nos forms

--- app/question/forms.py
+++ app/question/forms.py
@@ -1,9 +1,13 @@
-
-# forms.py
 from django import forms
-from .models import RespostaUsuario
+from .models import RespostaUsuario, Pergunta, Resposta
 
 class RespostaUsuarioForm(forms.ModelForm):
     class Meta:
         model = RespostaUsuario
         fields = ['pergunta', 'resposta']
+        
+
+    def __init__(self, id_da_pergunta, *args, **kwargs):
+        super(RespostaUsuarioForm, self).__init__(*args, **kwargs)
+        self.fields['pergunta'].queryset = Pergunta.objects.filter(id=id_da_pergunta)
+        self.fields['resposta'].queryset = Resposta.objects.filter(pergunta_id=id_da_pergunta)
